e4c6cec36e2d8d715ef182643d46529f58003c8a
muteb <mbm143105@gmail.com>
2026-05-13 17:18:05 +0300

    .

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,12 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+export const createClient = () => {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!url || !key) {
+    console.warn("Supabase credentials are missing!");
+  }
+
+  return createBrowserClient(url || '', key || '')
+}
